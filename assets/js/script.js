f2f5f45ac6b1e6303b7a10160f5ce50107920216
Vshepard879 <vshepard879@gmail.com>
2022-03-13 13:27:51 -0400

    changed the var to arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 // Assignment code here
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var upercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericalChar = "0123456789";
-var specialChar =" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+function generatePassword() {
+  // I created each character set separately as an array
+  var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+  var selectedArray = [];
 
+
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
